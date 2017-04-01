d43f773af9e37ff0b481a89af8fec19bcb9dbfce
Abdullah <s3478835@student.rmit.edu.au>
2017-04-01 20:59:50 +1100

    Modify landing page

--- jobmatch/resources/views/welcome.blade.php
+++ jobmatch/resources/views/welcome.blade.php
@@ -1,14 +1,23 @@
-@extends('layouts.app')
+@extends('layouts.homepageStyle')
 
 @section('content')
 <div class="container">
     <div class="row">
+        <br>
+        <br>
+        <br>
+        <h1><div style="text-align: center; color:rgba(0,0,0,0.82);" >Welcome</div></h1>
+        <br>
+        <br>
+        <br>
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+
+                <div class="panel-heading"</div>
 
                 <div class="panel-bodyLanding">
-                    Your Application's Landing Page.
+                    <a href="{{ url('/login') }}" class="button">Sign In</a><br>
+                    <a href="{{ url('/register') }}" class="button">Sign Up</a>
                 </div>
             </div>
         </div>
