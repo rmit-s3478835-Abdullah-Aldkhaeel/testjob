cbec782399d543c7783f3e91c9084dc39d59fabe
Abdullah <s3478835@student.rmit.edu.au>
2017-04-02 23:06:26 +1000

    Add footer

--- jobmatch/resources/views/welcome.blade.php
+++ jobmatch/resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <br>
         <br>
-        <h1><div style="text-align: center; color:rgba(0,0,0,0.82); " >Welcome...</div></h1>
+        <h1><div style="text-align: center; color:rgba(0,0,0,0.82); " >Welcome</div></h1>
         <br>
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
