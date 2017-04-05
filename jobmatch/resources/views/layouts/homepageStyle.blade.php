9d14ef99787e9de3a97dac0a0c3b1ebd8de54f68
Abdullah <s3478835@student.rmit.edu.au>
2017-04-05 10:05:24 +1000

    Modify navigation bar

--- jobmatch/resources/views/layouts/homepageStyle.blade.php
+++ jobmatch/resources/views/layouts/homepageStyle.blade.php
@@ -85,7 +85,7 @@
                 <ul class="nav nav-tabs nav-justified">
                     <li><a href="{{ url('/home') }}">Home</a></li>
                     <li><a href="{{ url('/about') }}">About</a></li>
-                    <li><a href="{{ url('/home') }}">Contact Us</a></li>
+                    <li><a href="{{ url('/contact') }}">Contact Us</a></li>
                     <!-- Authentication Links -->
                     @if (Auth::guest())
                         <li><a href="{{ url('/login') }}">Login</a></li>
