562b3ab5d6c8df5a8befb0f312fbd16ee55c07a0
Abdullah <s3478835@student.rmit.edu.au>
2017-04-10 20:19:40 +1000

    Display my account button in homepage after user loges in

--- jobmatch/resources/views/layouts/homepageStyle.blade.php
+++ jobmatch/resources/views/layouts/homepageStyle.blade.php
@@ -94,6 +94,7 @@
                         <li><a href="{{ url('/login') }}">Login</a></li>
                         <li><a href="{{ url('/register') }}">Register</a></li>
                     @else
+                        <li><a href="{{ url('/myAccount') }}">My Account</a></li>
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                 {{ Auth::user()->name }} <span class="caret"></span>
@@ -102,6 +103,7 @@
                             <ul class="dropdown-menu" role="menu">
                                 <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
                             </ul>
+
                         </li>
                     @endif
             </div>
