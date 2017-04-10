e3fe3685d036ee662750967617e66f327f05c5e4
Abdullah <s3478835@student.rmit.edu.au>
2017-04-10 20:11:51 +1000

    Redirect user to my account page after login

--- jobmatch/app/Http/Controllers/Auth/AuthController.php
+++ jobmatch/app/Http/Controllers/Auth/AuthController.php
@@ -28,7 +28,7 @@ class AuthController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = '/myAccount';
 
     /**
      * Create a new authentication controller instance.
