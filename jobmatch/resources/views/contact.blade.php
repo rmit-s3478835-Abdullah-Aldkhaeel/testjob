9203a9dc3f9d07e64e20dcd3d640bc17b7586caf
Ashish Jacob John <s3624455@student.rmit.edu.au>
2017-04-10 14:33:36 +1000

    Modify Contact page

--- jobmatch/resources/views/contact.blade.php
+++ jobmatch/resources/views/contact.blade.php
@@ -1,5 +1,20 @@
-@extends('layouts.beforeContentPage')
+@extends('layouts.staticPagesStyle')
+
 @section('content')
+    <div class="container">
+        <div class="row">
+            <br>
+            <br>
+            <br>
+            <br>
+            <div class="box">
+                <h3>{ Contact Us }</h3>
+                <p><span style="font-size: x-large; "></span> <br>Job Seeking, RMIT University</br>
+                    <br>Follow us on Facebook www.facebook.com/Job Seeking</br>
+                    <br>Follow us on instagram wwww.instagram./Job Seeking</br>
+                    <br>Follow us on LinkeddIn www.linkedin.com/Job Seeking</br></p>
+            </div>
 
-
+        </div>
+    </div>
 @endsection
